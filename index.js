53bd87fc074e02af4aaf5a47fac1a1ca50f8e175
sethvincent <sethvincent@gmail.com>
2017-02-15 17:11:28 -0800

    add parseMarkdown method

--- index.js
+++ index.js
@@ -5,52 +5,18 @@ function md2html (processor) {
   return function transformer (tree) {
     visit(tree,   function visitor (node, i, parent) {
       if (!node.children) return
+      var data = parseMarkdown(node, i, parent, tree)
 
-      if (node.type &&
-        node.type === 'linkReference' &&
-        node.children && node.children[0].value &&
-        parent.children[i + 1].value &&
-        parent.children[i + 1].value.match(/\{(.+)\}/).length
-      ) {
-        var text = node.children[0].value
-        var value = parent.children[i + 1].value
-        var data = {}
-
-        var idMatch = value.match(/\#\w+/)
-        var classMatch = value.match(/\.\w+(\-)?\w+/g)
-        var dataMatch = value.match(/([^=]\w+)=([^=]\w+\s*)/g)
-        var bracketMatch = value.match(/\{(.+)\}/)
-
-        var trailingText = value.split('}')[1]
-    
-        if (idMatch) {
-          var id = idMatch[0].replace('#', '')
-        }
-    
-        if (classMatch) {
-          var classNames = classMatch.map(function (cls) {
-            return cls.replace('.', '')
-          })
-        }
-    
-        if (dataMatch) {
-          dataMatch.map(function (item) {
-            var split = item.split('=')
-            var key = split[0].trim()
-            var val = split[1].trim()
-            data[key] = val
-          })
-        }
-
+      if (data) {
         parent.children[i] = {
           type: 'html',
-          value: createSpan(classNames, id, data, text)
+          value: data.html
         }
 
-        if (trailingText) {
+        if (data.trailingText) {
           parent.children[i + 1] = {
             type: 'text',
-            value: trailingText
+            value: data.trailingText
           }
         } else {
           remove(parent, parent.children[i + 1])
@@ -60,14 +26,73 @@ function md2html (processor) {
   }
 }
 
-function createSpan (classes, id, data, children) {
-  classes = classes ? classes.join(' ') : ''
+/*
+* if a bracket span statement is found: returns an object 
+* with id, classList, attr, and children properties
+* else returns false
+*/
+function parseMarkdown (node, i, parent, tree) {
+  if (!node.children) return false
 
-  var data = Object.keys(data).map(function (key) {
-    return `data-${key}="${data[key]}"`
+  if (node.type &&
+    node.type === 'linkReference' &&
+    node.children && node.children[0].value &&
+    parent.children[i + 1].value &&
+    parent.children[i + 1].value.match(/\{(.+)\}/).length
+  ) {
+    var text = node.children[0].value
+    var value = parent.children[i + 1].value
+    var data = {
+      id: null,
+      classList: [],
+      attr: {}
+    }
+
+    var idMatch = value.match(/\#\w+/)
+    var classMatch = value.match(/\.\w+(\-)?\w+/g)
+    var attrMatch = value.match(/([^=]\w+)=([^=]\w+\s*)/g)
+    var bracketMatch = value.match(/\{(.+)\}/)
+
+    if (idMatch) {
+      data.id = idMatch[0].replace('#', '')
+    }
+
+    if (classMatch) {
+      data.classList = classMatch.map(function (cls) {
+        return cls.replace('.', '')
+      })
+    }
+
+    if (attrMatch) {
+      attrMatch.map(function (item) {
+        var split = item.split('=')
+        var key = split[0].trim()
+        var val = split[1].trim()
+        data.attr[key] = val
+      })
+    }
+
+    data.text = text
+    data.trailingText = value.split('}')[1]
+    data.html = createSpan(data)
+    return data
+  } else  {
+    return false
+  }
+}
+
+function createSpan (data) {
+  var classes = data.classList ? data.classList.join(' ') : ''
+  var children = data.children
+  var text = data.text
+  var attrs = data.attrs
+  var id = data.id
+
+  var attr = Object.keys(data.attr).map(function (key) {
+    return `data-${key}="${data.attr[key]}"`
   }).join(' ')
 
-  return `<span${id ? ` id="${id}"` : ''} ${classes ? `class="${classes}"` : ''} ${data ? data : ''}>${children}</span>`
+  return `<span${id ? ` id="${id}"` : ''} ${classes ? `class="${classes}"` : ''} ${attr ? attr : ''}>${text}</span>`
 }
 
 function html2md () {
@@ -157,5 +182,6 @@ function mdVisitors (processor) {
 module.exports = {
   md2html: md2html,
   html2md: html2md,
-  mdVisitors: mdVisitors
+  mdVisitors: mdVisitors,
+  parseMarkdown: parseMarkdown
 }
